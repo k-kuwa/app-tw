cebb0fa198302eed3525d7df39cfe9d378812b13
k-kuwa <kyosuke@Kyosuke-no-MacBook-Pro.local>
2017-02-16 21:39:03 +0900

    create userpage

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,16 @@
 # TweetsControllerの定義
 class TweetsController < ApplicationController
   def index
-  end  
+    @tweets = Tweet.all.order('created_at DESC')
+
+  end
+
+  def create
+    Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:text, :image)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
   root 'tweets#index'
+  resources :tweets, only: %i(index new create)
   resources :users, only: %i(show)
 end
